5c401285aecd15c5356d2218e242eb076b6d55e1
Jeremiah David <jeremiahedavid@gmail.com>
2022-04-22 19:55:20 -0400

    added tinymce

--- config/settings/base.py
+++ config/settings/base.py
@@ -27,10 +27,7 @@ if READ_DOT_ENV_FILE:
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
 DEBUG = env.bool("DJANGO_DEBUG", False)
-if DEBUG:
-    PRODUCTION = env.bool("PRODUCTION", False)
-else:
-    PRODUCTION = True
+PRODUCTION = env.bool("PRODUCTION", False)
 
 # Local time zone. Choices are
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
