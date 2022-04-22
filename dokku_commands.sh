61906715bbbe7f9a5f60a69d65088ea4b026b368
Jeremiah David <jeremiahedavid@gmail.com>
2022-04-22 17:59:41 -0400

    added tinymce

--- dokku_commands.sh
+++ dokku_commands.sh
@@ -2,6 +2,6 @@
 
 # Running dokku commands for django manage.py
 
-python manage.py makemigrations && python manage.py migrate && python manage.py update_countries_plus
+python manage.py collectstatic && python manage.py makemigrations && python manage.py migrate && python manage.py update_countries_plus
 
 # Processes has ended
